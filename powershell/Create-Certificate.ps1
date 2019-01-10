2eb636083df92a52a063a6b1bfba8df5f88a0111
Pablo Brusseel <pablo.brusseel@hotmail.com>
2019-01-10 11:50:00 +0100

    Added Create-Certificate.ps1
    Create-Certificate creates self-signed certificates with PowerShell

---
+++ powershell/Create-Certificate.ps1
@@ -0,0 +1,17 @@
+﻿# Configuration
+
+$dns = 'example.com'
+$yearsvalid = 10
+$exportpath = 'C:\'
+$certname = 'cert' + '.pfx'
+$certpassword =  'password123'
+
+# End of Configuration
+
+$date_now = Get-Date
+$extended_date = $date_now.AddYears($yearsvalid)
+$cert = New-SelfSignedCertificate -certstorelocation cert:\localmachine\my -dnsname $dns -notafter $extended_date
+$pwd = ConvertTo-SecureString -String $certpassword -Force -AsPlainText
+$path = ‘cert:\localMachine\my\’ + $cert.thumbprint
+$fullexportpath = $exportpath + $certname
+Export-PfxCertificate -cert $path -FilePath $fullexportpath -Password $pwd
